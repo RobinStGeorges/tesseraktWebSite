e71ce33eb98a977d16935b825d127e418a3be4f9
robinstgeorges <robin.sgeorges@gmail.com>
2020-09-24 08:51:04 +0000

    login working

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,31 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session, Response
 from flask_sqlalchemy import SQLAlchemy
+import flask
 from flask import jsonify, flash
-import jwt
 import os
-import hashlib
 from flask_bcrypt import Bcrypt
 from flask_bcrypt import generate_password_hash, check_password_hash
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+import flask_login
+from flask_login  import LoginManager, UserMixin, login_required, login_user, logout_user
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField
+
+
+
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'thatmysecret'
+)
+bcrypt = Bcrypt(app)
 
-app.secret_key = 'thatsmysikritki'
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="Kireta",
@@ -28,6 +42,16 @@ db = SQLAlchemy(app)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
 class UserData(db.Model):
 
     __tablename__ = "userdata"
@@ -38,44 +62,84 @@ class UserData(db.Model):
     date_start = db.Column(db.DateTime)
     date_end = db.Column(db.DateTime)
 
-class User(db.Model):
 
-    __tablename__ = "user"
+class User(db.Model, UserMixin):
+    def __init__(self, emailUser, userPassword):
+        self.email = emailUser
+        self.password = userPassword
+        isAdmin = 0
+    __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    mail = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    token = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    isAdmin = db.Column(db.Integer)
 
-    def __init__(self, mail, password):
-        self.mail = mail
-        self.password = password
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    status = db.Column(db.String(100))
 
-    def hash_password(self):
-        self.password = generate_password_hash(self.password).decode('utf8')
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        userEmail = form.email.data
+        password = form.password.data
+        user=User.query.filter_by(email=userEmail).first()
+        if bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            return render_template('infos.html')
+        else:
+            return jsonify('erreur: 401')
+    else:
+        return render_template('login.html', form=form)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST':
+        userEmail = form.email.data
+        password = form.password.data
+        user=User.query.filter_by(email=userEmail).first()
+        if user is None:
+            user = User(userEmail, bcrypt.generate_password_hash(password))
+            db.session.add(user)
+            db.session.commit()
+            return render_template('infos.html')
+        else:
+            return jsonify('erreur: 401')
+    else:
+        return render_template('register.html', form=form)
 
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+
+@login_manager.user_loader
+def load_user(userEmail):
+    user=User.query.filter_by(email=userEmail).first()
+    return user
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/getToken/<user_mail>")
-def getTokenWithMail(user_mail):
-    user=User.query.filter_by(mail=user_mail).first()
-    id = user.id
-    return jsonify(encode_auth_token(id))
 
 @app.route("/setUserDataByMail", methods=["GET", "POST"])
 def setUserDataByMail():
@@ -87,46 +151,4 @@ def setUserDataByMail():
     if(user.verify_password(password)):
         return jsonify('c\'est okay')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(form.email.data, form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
 
-def encode_auth_token(user_id):
-    """
-    Generates the Auth Token
-    :return: string
-    """
-    try:
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-            'iat': datetime.datetime.utcnow(),
-            'sub': user_id
-        }
-        return jwt.encode(
-            payload,
-            SECRET_KEY,
-            algorithm='HS256'
-        )
-    except Exception as e:
-        return e
-
-@staticmethod
-def decode_auth_token(auth_token):
-    """
-    Decodes the auth token
-    :param auth_token:
-    :return: integer|string
-    """
-    try:
-        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-        return payload['sub']  #should return 1
-    except jwt.ExpiredSignatureError:
-        return 'Signature expired. Please log in again.'
-    except jwt.InvalidTokenError:
-        return 'Invalid token. Please log in again.'
---
+++ models.py
@@ -0,0 +1,16 @@
+# models.py
+
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    isAdmin = db.Column(db.Integer)
+
+class Order(db.model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100))
+    status = db.Column(db.String(100))
